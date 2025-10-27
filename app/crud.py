48286527ca288d3971204c9ee7becaa827e36d55
XxGtocxX <debanuj.de.official@gmail.com>
2025-10-27 10:48:22 +0530

    added content in app

---
+++ app/crud.py
@@ -0,0 +1,31 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+def get_products(db: Session):
+    return db.query(models.Product).all()
+
+def get_product(db: Session, product_id: int):
+    return db.query(models.Product).filter(models.Product.id == product_id).first()
+
+def create_product(db: Session, product: schemas.ProductCreate):
+    db_product = models.Product(name=product.name, price=product.price)
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+    return db_product
+
+def update_product(db: Session, product_id: int, product: schemas.ProductCreate):
+    db_product = get_product(db, product_id)
+    if db_product:
+        db_product.name = product.name
+        db_product.price = product.price
+        db.commit()
+        db.refresh(db_product)
+    return db_product
+
+def delete_product(db: Session, product_id: int):
+    db_product = get_product(db, product_id)
+    if db_product:
+        db.delete(db_product)
+        db.commit()
+    return db_product
